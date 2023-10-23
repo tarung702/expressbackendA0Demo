b315ca43ae96f3bda6bd2b582e4f6a77eee0d9cb
tarung702 <tarung702@gmail.com>
2023-10-23 15:43:30 -0400

    Commit 13 (I think?) modified:   routes/localsession-logout.js

--- routes/localsession-logout.js
+++ routes/localsession-logout.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-// Import the activeSessions object from app.js
-const { activeSessions } = require('../app');
-
 // Local Logout route
 router.get('/', (req, res) => {
   console.log('req.sessionID:', req.sessionID);
-  console.log('activeSessions keys:', Object.keys(activeSessions));
-  // Check if the user's session exists in activeSessions
-  if (req.sessionID in activeSessions) {
-    activeSessions[req.sessionID].destroy((err) => {
+  // Check if the user's local session exists
+  if (req.session) {
+    req.session.destroy((err) => {
       if (err) {
         console.error('Error destroying session:', err);
         res.status(500).json({ error: 'Internal server error' });
       } else {
-        console.log(`Session ${req.sessionID} destroyed locally.`);
+        console.log(`Local session ${req.sessionID} destroyed.`);
         res.status(200).json({ message: 'Logged out locally.' });
       }
     });
   } else {
-    res.status(200).json({ message: 'No active session found.' });
+    res.status(200).json({ message: 'No active local session found.' });
   }
 });
 
